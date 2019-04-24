9111bfd20c0d60c57d6602c3040657cbaeedab89
Jon Waggoner <jwaggs27@gmail.com>
2019-04-24 18:00:08 -0400

    heroku doesn't like secrets as files

--- server/__init__.py
+++ server/__init__.py
@@ -3,10 +3,13 @@ from .routes import *
 import firebase_admin
 from firebase_admin import credentials
 import os
+from flask import json
 
 
-firebase_json_file = os.getenv('MEATHEAT_FIREBASE')
-cred = credentials.Certificate(firebase_json_file)
+fir_secret = os.getenv('MEATHEAT_FIREBASE')
+if not os.path.isfile(fir_secret):
+    fir_secret = json.loads(fir_secret)
+cred = credentials.Certificate(fir_secret)
 firebase_app = firebase_admin.initialize_app(cred)
 
 print(firebase_app.name)
