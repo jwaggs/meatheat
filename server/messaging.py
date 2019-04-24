c4a02f257eda948b432a3975768b8cc3bee34b47
Jon Waggoner <jwaggs27@gmail.com>
2019-04-24 18:21:13 -0400

    key value dict iteration

--- server/messaging.py
+++ server/messaging.py
@@ -6,7 +6,7 @@ def send_data_to_device(data, device):
     app.logger.info(f'sending to device: {device}')
 
     sanitized = {}
-    for k, v in data:
+    for k, v in data.items():
         sanitized[k] = str(v)
 
     message = messaging.Message(
@@ -17,5 +17,5 @@ def send_data_to_device(data, device):
     production = app.config['FLASK_ENV'] == 'production'
     if production:
         message_id = messaging.send(message)
-        app.logger.info(f'successfully sent message to device: {device}')
+        app.logger.info(f'successfully sent message {message_id} to device: {device}')
 
