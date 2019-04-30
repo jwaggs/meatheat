6434566873d5c554fff6d345fd75689f4f7d56a3
Jon Waggoner <jwaggs27@gmail.com>
2019-04-29 20:09:44 -0400

    temp payload being displayed

--- ios/MeatHeat/MeatHeat/ViewController.swift
+++ ios/MeatHeat/MeatHeat/ViewController.swift
@@ -19,12 +19,35 @@ class ViewController: UIViewController {
     }
 
     @objc func onDidReceiveData(_ notification:Notification) {
-        guard let data = notification.userInfo?["data"] as? String else{
-            print("registration token not found in notification about itself")
+        guard let jsonString = notification.userInfo?["payload"] as? String else{
+            print("payload not found in notification about itself")
             return
         }
         
+        print(jsonString)
+        guard let jsonData = jsonString.data(using: .utf8) else {
+            print("could not parse json string into data")
+            return
+        }
         
+        do {
+            if let payload = try JSONSerialization.jsonObject(with: jsonData, options : .allowFragments) as? [Dictionary<String,Any>] {
+                handle(payload)
+            } else {
+                print("error: bad json")
+            }
+        } catch let error as NSError {
+            print("error serializing json data to array: \(error)")
+        }
+        
+    }
+    
+    func handle(_ payload: [Dictionary<String,Any>]) {
+        guard let temp = payload[0]["temp"] as? Float else {
+            print("warning: payload does not have temp")
+            return
+        }
+        LabelProbeCurrent.text = String(temp)
     }
 }
 
